85e0b0843fdebed9875509f1ae332d82ed9695e3
jsanderson104 <justin.sanderson104@gmail.com>
2026-09-25 12:43:48 -0500

    Fix UID variable and update image tagging

--- podman_build_image.bash
+++ podman_build_image.bash
@@ -3,14 +3,12 @@
 
 podman build -f Containerfile -t my-nginx:latest .
 
-
-
 # Find my Linux UID
-UID=$(getent passwd $(whoami) | cut -d: -f3)
+MYUID=$(getent passwd $(whoami) | cut -d: -f3)
 
 # Set Docker.io registry creds so I can push image
-cp /home/jenkins/workspace/Build-Nginx-Image/auth.json /run/user/1000/containers/auth.json
+cp /home/jenkins/workspace/Build-Nginx-Image/auth.json /run/user/$MYUID/containers/auth.json
 
 podman login docker.io || exit 1
 
-podman tag my-nginx docker.io/jsanderson104/stuff/my-nginx:latest
+podman tag my-nginx my-nginx:latest
