353d42e6399c89100730c6ca5e8bd0d842aea798
jsanderson104 <justin.sanderson104@gmail.com>
2026-09-25 20:20:50 -0500

    Comment out cleanup and test container setup commands
    Comment out cleanup commands and enable-linger for test container.

--- podman_build_image.bash
+++ podman_build_image.bash
@@ -16,14 +16,14 @@ podman tag my-nginx my-nginx:latest
 podman push localhost/my-nginx docker.io/jsanderson104/stuff:my-nginx
 
 # Cleanup old images and tags to make sure we can the most recent of everything.
-podman stop test ; 
-podman rm test ;
-podman rmi docker.io/library/nginx:latest
-podman rmi localhost/my-nginx
-podman rmi docker.io/jsanderson104/stuff:my-nginx
+#podman stop test ; 
+#podman rm test ;
+#podman rmi docker.io/library/nginx:latest
+#podman rmi localhost/my-nginx
+#podman rmi docker.io/jsanderson104/stuff:my-nginx
 
 
 
 # Run the new build as a container called "test"
-loginctl enable-linger 415600006
+#loginctl enable-linger 415600006
 #podman run -dt --name test -p 9090:9090 docker.io/jsanderson104/stuff:my-nginx
